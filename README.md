67c731a8c655439c5a52c9b20e9e3edcb8751331
Reco-P <1029832297@qq.com>
2020-06-05 22:40:18 +0800

    路由跳转进度条

--- README.md
+++ README.md
@@ -26,3 +26,10 @@ See [Configuration Reference](https://cli.vuejs.org/config/).
 ### 优化项
 ```
 1.路由懒加载
+
+
+### CDN
+```
+https://unpkg.com/nprogress@0.2.0/nprogress.js
+https://unpkg.com/nprogress@0.2.0/nprogress.css
+```
--- src/main.js
+++ src/main.js
@@ -10,14 +10,24 @@ import VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css' // import styles
 import 'quill/dist/quill.snow.css' // for snow theme
 import 'quill/dist/quill.bubble.css' // for bubble theme
+// 导入切换路由时的进度条
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
 // 导入axios
 import axios from 'axios'
 // 配置请求的根路径
 // axios.defaults.baseURL = '/api/'     //baseURL 的运行层级高于代理URL,如果存在baseURL,则不运行代理服务
 
 // 为每一次请求添加请求拦截器
+// 在request拦截器中 展示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
+// 在response拦截器中 隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 
